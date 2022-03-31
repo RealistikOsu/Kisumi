5b74e3281af45522329617f610c7f5f5756c403d
RealistikDash <realistikdash@gmail.com>
2022-03-31 20:12:59 +0200

    mysql context wrappers, pools etc

--- Kisumi/db/mysql.py
+++ Kisumi/db/mysql.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import (
     Any,
     Union,
@@ -54,3 +55,74 @@ class MySQLConnection:
         row = await self.fetchone(query, args)
 
         return row[0] if row else None
+
+class _MySQLCursorIter:
+    """An iterable result allowing for using large result sets memory
+    efficiently."""
+
+    __slots__ = (
+        "_cur",
+    )
+
+    def __init__(self, cur: aiomysql.Cursor) -> None:
+        self._cur = cur
+    
+    async def __aiter__(self) -> _MySQLCursorIter:
+        return self
+    
+    async def __anext__(self) -> tuple[Any, ...]:
+        res = await self._cur.fetchone()
+        if not res:
+            raise StopAsyncIteration
+        return res
+    
+    # Destructor
+    #def __del__(self) -> None:
+    #    self._cur.close()
+
+class MySQLPool:
+    """A thin wrapper around the aiomysql connection pool with the purpose of 
+    yielding only `MySQLConnection` instances."""
+
+    __slots__ = (
+        "_pool",
+    )
+
+    def __init__(self, conn: aiomysql.Pool) -> None:
+        """Creates an instance of `MySQLPool` from an existing pool."""
+
+        self._pool = conn
+    
+    async def acquire_connection(self) -> MySQLConnection:
+        """Acquires the MySQL connection from the pool, waiting until there
+        is a free one."""
+
+        return MySQLConnection(
+            await self._pool._acquire(),
+        )
+    
+    async def release_connection(self, conn: MySQLConnection) -> None:
+        """Releases a connection from the pool."""
+
+        await self._pool.release(conn._conn)
+    
+    async def acquire(self) -> _MySQLAcquireContextManager:
+        """Acquires a MySQL connection using a context manager."""
+
+# Heavily inspired by aiomysql's context managers.
+class _MySQLAcquireContextManager:
+    """A context manager for the acquisition of MySQL connections from a
+    pool."""
+
+    __slots__ = (
+        "_pool",
+    )
+
+    def __init__(self, pool: MySQLPool) -> None:
+        self._pool = pool
+    
+    async def __aenter__(self) -> MySQLConnection:
+        return await self._pool.acquire_connection()
+    
+    async def __aexit(self, *_) -> None:
+        return await self._pool.release_connection()
